bb9d6be28343884478bc690cc131cc2df7e451d6
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-09-30 23:21:05 +0530

    js added

---
+++ q1/index.js
@@ -0,0 +1,6 @@
+// Write your code below:
+let fruitList = document.getElementsByClassName("fruit");
+fruitList[2].style.backgroundColor = "yellow";
+for (let i = 0; i < fruitList.length; i++){
+    fruitList[i].style.fontWeight = "bold";
+}
