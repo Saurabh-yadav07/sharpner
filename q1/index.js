0f2b8bcb1abe11bd5a2586abcfe168290c088c76
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-10-01 14:45:48 +0530

    getElementsByTagName

--- q1/index.js
+++ q1/index.js
@@ -1,6 +1,6 @@
 // Write your code below:
-let fruitList = document.getElementsByClassName("fruit");
-fruitList[2].style.backgroundColor = "yellow";
-for (let i = 0; i < fruitList.length; i++){
-    fruitList[i].style.fontWeight = "bold";
+let list = document.getElementsByTagName("li");
+list[4].style.color = "blue";
+for (let i = 0; i < list.length; i++){
+    list[i].style.fontStyle = "italic";
 }
