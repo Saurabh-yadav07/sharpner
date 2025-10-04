e13757c5720a5d559889e84b45f63776ea9c6ae1
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-10-04 11:07:11 +0530

    today Added edit and delete fruits functionality

--- q1/index.js
+++ q1/index.js
@@ -1,12 +1,28 @@
-// Write your code below:
-const header = document.querySelector("#header");
-const subHeading = document.createElement("h3");
-subHeading.textContent = "Buy high quality organic fruits online";
-subHeading.style.fontStyle = "italic";
-header.appendChild(subHeading);
+// Add the Edit Button:
 
-const fruitList = document.querySelector(".fruits");
-const para = document.createElement("p");
-para.textContent = "Total fruits:4";
-para.id = "fruits-total";
-fruitList.before(para);
+
+// Implement the code as in video but with one extra 'Edit' button in 'li'
+
+const form = document.querySelector("form");
+const fruits = document.querySelector(".fruits");
+const existingFruits = document.querySelectorAll('.fruits .fruit');
+existingFruits.forEach((li) => {
+    const editBtn = document.createElement('button');
+    editBtn.className = 'edit-btn';
+    editBtn.textContent = 'Edit';
+    li.appendChild(editBtn);
+})
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const fruitToAdd = document.getElementById("fruit-to-add")
+    const newLi = document.createElement("li");
+    newLi.className = 'fruit';
+    newLi.innerHTML = `${fruitToAdd.value} <button class="delete-btn">x</button><button class='edit-btn'>Edit</button>`;
+    fruits.appendChild(newLi);
+});
+fruits.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-btn")) {
+        const fruitTobeDeleted = event.target.parentElement;
+        fruits.removeChild(fruitTobeDeleted);
+    }
+});
